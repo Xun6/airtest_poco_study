730bd2d8ce0164192ac1cb10ae04960afa9cb9f9
xiaoyu <yuxunzhi@yongliweb.com>
2019-11-12 09:30:48 +0800

    提交套件测试实例练习

---
+++ Test_Ng_git/src/com/test/git/testNG/TestName.java
@@ -0,0 +1,55 @@
+package com.test.git.testNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+/*annotations ：注释、注解*/
+
+public class TestName {
+	
+	@Test
+	public void testCase1(){
+		System.out.println("这是测试用例一");
+	}
+	
+	@Test
+	public void testCase2(){
+		System.out.println("这是测试用例二");
+	}
+	
+	@BeforeMethod
+	public void BeforeMethod(){
+		System.out.println("------------------------");  /*在每一个测试方法之前运行*/
+	}
+	
+	@AfterMethod
+	public void AfterMethod(){
+		System.out.println("++++++++++++++++++++++++++"); /*在每一个测试方法之后运行*/
+	}
+	
+	@BeforeClass
+	public void BeforeClass(){
+		System.out.println("在当前类的第一个测试方法调用前运行");
+	}
+	
+	@AfterClass
+	public void AfterClass(){
+		System.out.println("在当前类所有测试方法调用后运行");
+	}
+	
+	@BeforeSuite
+	public void BeforeSuite(){
+		System.out.println("在所有测试运行之前运行");
+	}
+	
+	@AfterSuite
+	public void AfterSuite(){
+		System.out.println("在所有测试运行之后运行");
+	}
+				
+}
---
+++ Test_Ng_git/src/com/test/git/testNG/suite/PayTest.java
@@ -0,0 +1,12 @@
+package com.test.git.testNG.suite;
+
+import org.testng.annotations.Test;
+
+public class PayTest {
+	
+	@Test
+	public void Pay(){
+		System.out.println("支付运行成功了~~~");
+	}
+	
+}
